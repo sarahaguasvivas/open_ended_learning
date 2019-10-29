41e56a967474c08f6fc4d4e42ccbb40c1d5d6f7c
sarahaguasvivas <saag5228@colorado.edu>
2019-10-29 14:54:13 -0600

    updating

---
+++ datastructures/dag.py
@@ -0,0 +1,14 @@
+import networkx as nx
+
+class Node:
+    def __init__(self, nodeid : str, data):
+        self.nodeid = nodeid
+        self.data = data
+
+class DAG:
+    def __init__(self):
+        pass
+
+
+    def add_edge(self):
+        pass
