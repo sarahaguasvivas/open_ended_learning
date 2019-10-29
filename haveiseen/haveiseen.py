db79900eb2ed18f86e04dfb0870b468ef8da36a1
sarahaguasvivas <saag5228@colorado.edu>
2019-10-29 13:32:26 -0600

    working on setup

--- haveiseen/haveiseen.py
+++ haveiseen/haveiseen.py
@@ -6,11 +6,16 @@ HaveISeen:
     existing knowledge or if it should be
     added as new knowledge.
 """
-
+from abc import ABC, abstractmethod
 
 class HaveISeen():
 
     def __init__(self):
         pass
+
+    @abstractmethod
+    def check(self):
+        pass
+
 
 
