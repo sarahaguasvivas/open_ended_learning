226c7995c275ca506454c704284446b65d375b82
Michael Lauria <michael.david.lauria@gmail.com>
2019-10-30 14:13:25 -0600

    wip

--- test_mnist.py
+++ test_mnist.py
@@ -4,7 +4,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import ipdb
+# import numpy as np
+# from sklearn.model_selection import StratifiedShuffleSplit
 
+MAX_TEST_CLASSES = 3
+MAX_TRAIN_CLASSES = 2
 
 class Net(nn.Module):
     def __init__(self):
@@ -12,7 +17,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
         self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.fc2 = nn.Linear(500, MAX_TRAIN_CLASSES)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -22,11 +27,16 @@ class Net(nn.Module):
         x = x.view(-1, 4*4*50)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        softmax = F.log_softmax(x, dim=1)
+        return softmax
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+        # restrict data. TODO: do this in DataLoader
+        data = data[target < MAX_TRAIN_CLASSES]
+        target = target[target < MAX_TRAIN_CLASSES]
+
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
@@ -44,6 +54,10 @@ def test(args, model, device, test_loader):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
+            # restrict data. TODO: do this in DataLoader
+            data = data[target < MAX_TEST_CLASSES]
+            target = target[target < MAX_TEST_CLASSES]
+
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
@@ -101,8 +115,6 @@ def main():
     ]))
     test_loader = torch.utils.data.DataLoader(test_dataset,
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
-
-
 
     model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
